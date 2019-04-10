1788b286042acb1b577c924514da9c5cabf9b093
Baldeep Singh <itsbaldeep@gmail.com>
2019-04-10 17:45:25 +0530

    Added about and skills section

--- js/script.js
+++ js/script.js
@@ -12,5 +12,32 @@ $(document).ready(function() {
         loop: true,
         showCursor: false
     });
-
+    
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: { items: 1 },
+            480: { items: 2 },
+            768: { items: 3 },
+            938: { items: 4 }
+        }
+    });
+    
+    const skillsTopOffset = $('.skills-section').offset().top;
+    $(window).scroll(function() {
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+    });
 })
